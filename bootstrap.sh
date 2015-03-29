afca0402d1c88be608fa71abffefa2aa82c3188a
Hugo Stijns <h.stijns@coolblue.nl>
2015-03-29 17:05:27 +0200

    Do not create links if they already exist

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,12 +2,23 @@
 
 cd "$(dirname "${0}")";
 
+create_link() {
+  local src=${1} dst=${2}
+  local currentSrc="$(readlink ${dst})"
+
+  if [ "$currentSrc" == "$src" ]; then
+    return
+  fi
+
+  ln -sf "${src}" "${dst}"
+}
+
 echo "Updating..."
 git pull origin master
 
 echo "Creating symlinks..."
-for src in $(find * -depth 1 -name '*'); do
-  ln -sf ".dotfiles/${src}" "${HOME}/.$(basename ${src})"
+for src in $(find * -depth 1 -name "*" -not -path "sublime/*"); do
+  create_link ".dotfiles/${src}" "${HOME}/.$(basename ${src})"
 done
 
 echo "Done"
